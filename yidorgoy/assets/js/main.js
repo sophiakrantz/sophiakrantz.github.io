ab6b21d3184804b5050f7ef4352eddf8ef7392e1
Sophia Krantz <sophia.a.krantz@gmail.com>
2020-11-04 23:47:26 -0500

    new corrections

--- yidorgoy/assets/js/main.js
+++ yidorgoy/assets/js/main.js
@@ -1,3 +1,4 @@
+//Home page
 $(function(){ 
 	var oldtext;     
 	$('#yid').hover(function(){ 
@@ -30,6 +31,80 @@ $(function(){
 	$('#goy').text(function(){ 
 		oldtext = $(this).text(); 
 		$(this).text("Goy"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+})
+
+//Yid page
+$(function(){ 
+	var oldtext;     
+	$('#shidduch').hover(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Matchmaking"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+}) 
+$(function(){ 
+	var oldtext;     
+	$('#shidduch').text(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Shidduch"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+})
+$(function(){ 
+	var oldtext;     
+	$('#schnitzel').hover(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Food"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+}) 
+$(function(){ 
+	var oldtext;     
+	$('#schnitzel').text(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Schnitzel"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+})
+$(function(){ 
+	var oldtext;     
+	$('#schneerson').hover(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Religion"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+}) 
+$(function(){ 
+	var oldtext;     
+	$('#schneerson').text(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Schneerson"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+})
+$(function(){ 
+	var oldtext;     
+	$('#shtick').hover(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Gimmick"); 
+	}, function(){ 
+		$(this).text(oldtext) 
+	}); 
+}) 
+$(function(){ 
+	var oldtext;     
+	$('#shtick').text(function(){ 
+		oldtext = $(this).text(); 
+		$(this).text("Shtick"); 
 	}, function(){ 
 		$(this).text(oldtext) 
 	}); 
