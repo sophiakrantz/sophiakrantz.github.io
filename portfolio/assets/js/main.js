46aed88a0de6905b167f268e6c0ff535dbab80f8
Sophia Krantz <sak305@scarletmail.rutgers.edu>
2021-04-07 00:00:16 -0400

    fucked with some shit

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -3,12 +3,3 @@ $(function() {
 		$(".about").toggleClass("hide");
 	});
 });
-
-
-// $(function() {
-// 	let w = $(window).width();
-// 	let h = $(window).height();
-// 	if(h > w){
-// 		alert("for best experience view site on desktop")
-// 	}
-// })
