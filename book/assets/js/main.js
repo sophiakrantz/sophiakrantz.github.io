3ddb3391f65078357cfa15d32857cb2033a432bb
Sophia Krantz <sak305@scarletmail.rutgers.edu>
2021-04-10 15:41:40 -0400

    book added panels

--- book/assets/js/main.js
+++ book/assets/js/main.js
@@ -1,10 +1,49 @@
 $(function() {
-	$( ".circle" ).click(function() {
-		$(".closeup").toggleClass("hide");
+	$( ".circle1" ).click(function() {
+		$(".excerpt1").toggleClass("hide");
 	});
 });
 $(function() {
-	$( ".closeup" ).click(function() {
-		$(".closeup").toggleClass("hide");
+	$( ".excerpt1" ).click(function() {
+		$(".excerpt1").toggleClass("hide");
 	});
 });
+$(function() {
+	$( ".circle2" ).click(function() {
+		$(".excerpt2").toggleClass("hide");
+	});
+});
+$(function() {
+	$( ".excerpt2" ).click(function() {
+		$(".excerpt2").toggleClass("hide");
+	});
+});
+$(function() {
+	$( ".circle3" ).click(function() {
+		$(".excerpt3").toggleClass("hide");
+	});
+});
+$(function() {
+	$( ".excerpt3" ).click(function() {
+		$(".excerpt3").toggleClass("hide");
+	});
+});
+$(function() {
+	$( ".circle4" ).click(function() {
+		$(".excerpt4").toggleClass("hide");
+	});
+});
+$(function() {
+	$( ".excerpt4" ).click(function() {
+		$(".excerpt4").toggleClass("hide");
+	});
+});
+
+
+$(function() {
+	let w = $(window).width();
+	let h = $(window).height();
+	if(h > w){
+		alert("For optimal experience, please view site in landscape mode.")
+	}
+})
