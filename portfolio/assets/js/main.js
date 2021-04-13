2c4acdbaba632422c10c65973a8a3dd8b9345739
Sophia Krantz <sak305@scarletmail.rutgers.edu>
2021-04-13 07:18:02 -0400

    Individual pages added

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -11,16 +11,16 @@ $(function() {
 		$(".open").toggleClass("reveal");
 	});
 });
+$(function() {
+	$( ".exit" ).hover(function() {
+		$(".closed").toggleClass("hide");
+		$(".open").toggleClass("reveal");
+	});
+});
 
-// $(function() {
-// 	$( ".email").click(function() {
-// 		$(".email_address").show();
-// 		$(".email_address").removeClass("hide");
-// 		$(".instagram_address").hide();
-// 		$(".instagram_address").removeClass("reveal");
-// 		$(".twitter_address").hide();
-// 		$(".twitter_address").removeClass("reveal");
-// 		$(".youtube_address").hide();
-// 		$(".youtube_address").removeClass("reveal");
-// 	});
-// });
+$(function() {
+	$( ".next").click(function() {
+		$(".indium").toggleClass("hide");
+		$(".previous").toggleClass("hide");
+	});
+});
