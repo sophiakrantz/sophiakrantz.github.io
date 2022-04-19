99cea256537b5fbc662ef5d7312217aaa386d442
Sophia Krantz <sak305@scarletmail.rutgers.edu>
2022-04-18 22:28:48 -0400

    override

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,42 @@
+// $(function() {
+// 	$( ".hover" ).hover(function() {
+// 		$(".media").removeClass("hide");
+// 	});
+// });
+
 $(function() {
-	$( ".truth" ).click(function() {
-		$("span").removeClass("hide");
-		$("span").toggleClass("reveal");
-	});
-});
+	$(".print").click(function() {
+		$(".directory").addClass("hide");
+		$(".print_directory").removeClass("hide");
+		$(".digital_directory").addClass("hide");
+		$(".aboutme").addClass("hide");
+		$(".arrow").removeClass("hide");
+	})
+})
+$(function() {
+	$(".digital").click(function() {
+		$(".directory").addClass("hide");
+		$(".print_directory").addClass("hide");
+		$(".digital_directory").removeClass("hide");
+		$(".aboutme").addClass("hide");
+		$(".arrow").removeClass("hide");
+	})
+})
+$(function() {
+	$(".about").click(function() {
+		$(".directory").addClass("hide");
+		$(".print_directory").addClass("hide");
+		$(".digital_directory").addClass("hide");
+		$(".aboutme").removeClass("hide");
+		$(".arrow").removeClass("hide");
+	})
+})
+$(function() {
+	$(".arrow").click(function() {
+		$(".directory").removeClass("hide");
+		$(".print_directory").addClass("hide");
+		$(".digital_directory").addClass("hide");
+		$(".aboutme").addClass("hide");
+		$(".arrow").addClass("hide");
+	})
+})
